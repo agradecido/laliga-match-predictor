eb892732e1f4e4bf9351bffe9a93207cc88b22a8
Javier Sierra <javier.sierra@oc.vasscompany.com>
2026-07-30 15:00:22 +0200

    fix: minor errors

--- .devcontainer/node/entrypoint.sh
+++ .devcontainer/node/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+echo "Waiting for .env to be ready..."
+until [ -f .env ] && grep -q "^APP_KEY=base64" .env; do
+    sleep 1
+done
+
 if [ ! -d node_modules ] || [ ! -f node_modules/.bin/vite ]; then
     echo "Installing npm dependencies..."
     npm install
